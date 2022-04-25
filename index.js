0e3960855b2ef1a9cf8e57c814c9b221f12a5ae8
Uenderss <meajudabarto@gmail.com>
2022-04-25 02:28:21 -0300

    requisitos basicos

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-import express, { response } from "express";
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import chalk from "chalk";
+import crypto from 'crypto';
 
-const usuario=[
+
+
+const usuarios = [
     {
+      
         username: 'bobesponja', 
         avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    },
-    {
-        username: 'patrick', 
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
     }
+];
 
-]
-const app=express();
+const tweetsLista = [
+  {
+   
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+];
+
+const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.get("/sign-up",(req,resp)=>{
+app.post("/sign-up", (req, resp) => {
+  const { username, avatar } = req.body;
+  const usuario = {
+    username,
+    avatar
+  };
 
+  usuarios.push(usuario);
+
+  return resp.json({ message: "OK" });
+});
+
+app.post("/tweets", (req, resp) => {
+    const { username, tweet } = req.body;
+    const tweets = {
+      username,
+      tweet,
+    };
+    
+    tweetsLista.push(tweets);
+  
+    return resp.json({ message: "OK" });
+  });
+
+app.get("/tweets",(req,resp)=>{
+    return resp.json(tweetsLista);
 })
-app.get("/informeEndero/:idParametro/categoria/:outrasIds",(req,resp)=>{
-    const {idParametro,outrasIds}=req.params;
-    resp.send("bem vindo")
-})
 
-app.listen(5000,console.log(chalk.blue("conectado com sucesso")));
+app.listen(5000, console.log(chalk.blue("conectado com sucesso")));
